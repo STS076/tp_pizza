a26bf14fc54122c5ec6a3a59393b0c045c51b06e
Sophie Toussaint <sophfrom76@hotmail.com>
2024-02-15 16:33:29 +0100

    function get orders

--- app/service/OrderService.js
+++ app/service/OrderService.js
@@ -2,27 +2,39 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb://sophie_toussaint:caolso14vylb@localhost:27018/";
 const client = new MongoClient(uri);
 
-async function getOrdersByPizza(name) {
-    const database = client.db('pizzas_orders_db');
-    const orders = database.collection('orders');
 
-    const query = { name: name };
-    const order = await orders.findOne(query);
-    console.log(order);
+class OrderService {
+    getOrdersByPizza(name) {
+        const database = client.db('pizzas_orders_db');
+        const orders = database.collection('orders');
 
+        const query = { name: name };
+        const order = orders.find(query);
+        order.forEach(console.log);
+    }
+
+
+    getOrdersBySize(size) {
+        const database = client.db('pizzas_orders_db');
+        const orders = database.collection('orders');
+
+        const query = { size: size };
+        const order = orders.find(query);
+        order.forEach(console.log);
+    }
+
+
+    getOrders() {
+        const db = client.db("pizzas_orders_db");
+        const coll = db.collection("orders");
+
+        const orders = coll.find();
+        // iterate code goes here
+        orders.forEach(console.log);
+    }
 }
 
-async function getOrdersBySize(size) {
-    const database = client.db('pizzas_orders_db');
-    const orders = database.collection('orders');
-
-    const query = { size: size };
-    const order = await orders.findOne(query);
-    console.log(order);
-}
-
-
-async function getOrders(){
-
-    
-}
+const order = new OrderService()
+console.log( order.getOrdersByPizza("Pepperoni") )
+console.log( order.getOrdersBySize("small") )
+console.log( order.getOrders() )
