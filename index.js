3a15d9582da095d9f18fa6e63d5936767e8784d0
Nguyen Juventin <juventin.ng@gmail.com>
2024-02-15 16:49:42 +0100

    commit before pull init

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // Replace the uri string with your connection string.
-const uri = "mongodb://sophie_toussaint:caolso14vylb@localhost:27018/";
+const uri = "mongodb://LaMouettas:root@localhost:27018/";
 
 const client = new MongoClient(uri);
 
