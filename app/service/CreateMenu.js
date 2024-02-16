8bf3ed5f9c81fdf8790fb170e7687205cb5a0bc8
Sophie Toussaint <sophfrom76@hotmail.com>
2024-02-16 15:27:31 +0100

    create menu

---
+++ app/service/CreateMenu.js
@@ -0,0 +1,11 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const uri = "mongodb://sophie_toussaint:caolso14vylb@localhost:27018";
+const client = new MongoClient(uri);
+
+class CreateMenu {
+
+
+}
+
+const menu = new CreateMenu()
