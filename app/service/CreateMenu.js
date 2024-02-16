01e30ff182d3f8e7ae621061035eabc12d8052a2
Sophie Toussaint <sophfrom76@hotmail.com>
2024-02-16 16:18:42 +0100

    create menu

--- app/service/CreateMenu.js
+++ app/service/CreateMenu.js
@@ -2,10 +2,31 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const uri = "mongodb://sophie_toussaint:caolso14vylb@localhost:27018";
 const client = new MongoClient(uri);
+const fs = require("fs");
 
-class CreateMenu {
 
+async function createMenuCollection() {
+    const database = client.db("pizzas_orders_db");
+    const orders = database.collection("orders");
 
+    const menu = await orders.aggregate([
+        {
+            $group: {
+                _id: "$pizza",
+                menuItems: {
+                    $push: {
+                        _id: "$_id",
+                        name: "$name",
+                        size: "$size",
+                        price: "$price",
+                    },
+                },
+            },
+        },
+    ]).toArray();
+
+    const menuJSON = JSON.stringify(menu);
+    fs.writeFileSync("menu.json", menuJSON);
 }
 
-const menu = new CreateMenu()
+createMenuCollection();
