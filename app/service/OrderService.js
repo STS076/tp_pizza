ad53efe2c4f30c68ff574290abf1de47256ac92e
Nguyen Juventin <juventin.ng@gmail.com>
2024-02-16 14:19:02 +0100

    feat(order): mostPopularPizzaSize func

--- app/service/OrderService.js
+++ app/service/OrderService.js
@@ -99,6 +99,34 @@ class OrderService {
     console.log(largeOrders);
   }
 
+  async getMostPopularPizzaSize() {
+    const db = client.db("pizzas_orders_db");
+    const coll = db.collection("orders");
+  
+    // Convertir le cursor en tableau pour pouvoir utiliser await
+    const orders = await coll.find().toArray();
+    let salesCount = {
+      small: { quantity: 0 },
+      medium: { quantity: 0 },
+      large: { quantity: 0 },
+    };
+  
+    // Calculer le total des quantités vendues pour chaque taille de pizza
+    orders.forEach((order) => {
+      if (salesCount[order.size]) {
+        salesCount[order.size].quantity += order.quantity;
+      }
+    });
+  
+    // Afficher les résultats de salesCount pour débogage
+    console.log(salesCount);
+  
+    // Trouver la taille de pizza la plus vendue
+    return Object.keys(salesCount).reduce((a, b) =>
+      salesCount[a].quantity > salesCount[b].quantity ? a : b
+    );
+  }
+
   async getMostProfitablePizza() {
     const db = client.db("pizzas_orders_db");
     const coll = db.collection("orders");
@@ -129,5 +157,11 @@ const order = new OrderService();
 // console.log("totalOrders: ", order.getTotalOrders())
 // console.log("Vegan orders : ", order.getVeganPizza("Vegan"))
 // console.log("Large orders : ", order.getLargePizza("large"))
-console.log("Recette : ", order.getMostProfitablePizza());
-// console.log(uri);
+// console.log("Recette : ", order.getMostProfitablePizza());
+order.getMostPopularPizzaSize()
+  .then(mostPopularSize => {
+    console.log(`La taille de pizza la plus populaire est : ${mostPopularSize}`);
+  })
+  .catch(error => {
+    console.error(error);
+  });
